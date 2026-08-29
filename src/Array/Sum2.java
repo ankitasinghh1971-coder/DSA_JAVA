4a2076b259dc34201d0a192fa98700839245c1f9
ankitasinghh1971-coder <ankitasinghh1971@gmail.com>
2026-08-29 18:02:08 +0530

    2 sum

---
+++ src/Array/Sum2.java
@@ -0,0 +1,34 @@
+package Array;
+
+import java.util.*;
+
+public class Sum2 {
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+
+                System.out.println(
+                        "Indices: " + map.get(complement) + ", " + i
+                );
+
+                System.out.println(
+                        "Values: " + complement + ", " + nums[i]
+                );
+
+                break;
+            }
+
+            map.put(nums[i], i);
+        }
+    }
+}
